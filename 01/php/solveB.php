648b6f99f4abca9ac1a6044ccdaff21ceaf6c88b
Matthew Hepburn <matthewhepburn0@gmail.com>
2021-12-04 13:55:56 +0000

    fix off-by-one in 01/php/solveB.php

--- 01/php/solveB.php
+++ 01/php/solveB.php
@@ -1,6 +1,9 @@
 <?php
 
 require_once __DIR__ . '/../../common/php/InputLoader.php';
+require_once __DIR__ . '/../../common/php/Logger.php';
+
+$logger = new Logger();
 
 $depths = (new InputLoader(__DIR__))->getAsInts();
 
@@ -12,11 +15,12 @@ function getWindowAt(int $i, array $array, int $windowSize) {
 
 $lastWindowSum = array_sum(getWindowAt(0, $depths, $windowSize));
 $depthIncreases = 0;
-for ($i = 1; $i < count($depths) - $windowSize; $i++) {
+for ($i = 1; $i < count($depths) - $windowSize + 1; $i++) {
     $windowSum = array_sum(getWindowAt($i, $depths, $windowSize));
     if ($windowSum > $lastWindowSum) {
         $depthIncreases += 1;
     }
+    $logger->log("Window has depth $windowSum, last window was $lastWindowSum");
     $lastWindowSum = $windowSum;
 }
 
